4a352ce29fe7f7962aaa505ab3f7d93c89badbdd
Asimi1234 <ayamayomide@gmail.com>
2025-01-21 08:22:39 +0100

    week 1 home page assignment

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+    
+// Populate the last modified date
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
