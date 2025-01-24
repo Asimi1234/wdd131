8d04ae7f1170d9a565b0397118246fef51b56acd
Asimi1234 <ayamayomide@gmail.com>
2025-01-24 04:54:19 +0100

    week 2 assignment

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,11 +1,7 @@
 // temples.js
 
 document.getElementById("currentyear").textContent = new Date().getFullYear();
-    
-// Populate the last modified date
 document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
-
-// Hamburger menu toggle
 const hamburgerButton = document.getElementById('hamburger-menu');
 const navMenu = document.querySelector('.nav-menu');
 
